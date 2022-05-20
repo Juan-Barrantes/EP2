6d7f3858e72787d7d2f2e4554de70bce7b4590b5
Juan Barrantes <barrantesjuan513@gmail.com>
2022-05-20 08:10:11 -0500

    add html structure

--- src/amortizacion.js
+++ src/amortizacion.js
@@ -14,24 +14,55 @@ function loadScreen (){
     const h2Tag = document.createElement('h2');
     h2Tag.innerHTML = 'Calcule su amortización con el método francés';
 
-    let inputsList = ['Monto','Tiempo (en meses)', 'Interés mensual'];
+    
+    let inputsList = [
+        {
+            labelFor:'monto',
+            labelInner:'Monto',
+            inputID: 'monto',
+            inputPH: 'Ingresar monto'
+        },
+        {
+            labelFor:'tiempo',
+            labelInner:'Meses',
+            inputID: 'tiempo',
+            inputPH: 'N° meses'
+        },
+        {
+            labelFor:'interes',
+            labelInner:'Monto',
+            inputID: 'monto',
+            inputPH: 'Tasa de interés mensual'
+        }
+    ];
+
+    let labelInner = ['Monto','Tiempo (en meses)', 'Interés mensual'];
+
     let labelFor = ['monto','text','text'];
     
     // appendChild
     body.appendChild(container);
     container.appendChild(subContainer);
+    subContainer.appendChild(containerForm);
     containerForm.appendChild(h2Tag);
-
+    
 
     inputsList.forEach(item =>{
         const divForm = document.createElement('div');
-        //divForm.classList.add=('form-group');    
+        divForm.classList.add=('form-group');    
         const labelForm =document.createElement('label');
         //labelForm.for
         const inputForm = document.createElement('input');
 
-        divForm.appendChild(labelForm,inputForm);
+        //labelFor.=item;
+        
+        containerForm.appendChild(divForm)
+        divForm.appendChild(labelForm);
+        divForm.appendChild(inputForm);   
     })
+
+
+
     const buttonCalc = document.createElement('button');
     
     console.log('Funcó la vaina');
