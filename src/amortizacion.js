6fda0b96498a0403b6641c721d263c60b011daec
Juan Barrantes <barrantesjuan513@gmail.com>
2022-05-21 08:49:54 -0500

    add form, add table and styles

--- src/amortizacion.js
+++ src/amortizacion.js
@@ -1,72 +1,73 @@
+
 function loadScreen (){
     const body = document.querySelector('body');
     
     const container = document.createElement('div');
-    container.classList.add=('container');
+    container.classList.add=("container");
+    container.style.margin = '20px 5%';
+    container.style.padding = '30px';
+    container.style.border = 'solid 1px'
+    container.style.borderRadius = '20px'
+    
 
     const subContainer = document.createElement('div');
-    subContainer.classList.add=['row' ,'mt-3'];
+    subContainer.classList.add=("row" ,"mt-3");
+    subContainer.style.display = 'flex';
 
     const containerForm = document.createElement('div');
-    containerForm.classList.add=('col-6');
+    containerForm.classList.add=("col-6");
     
+    const containerTable = document.createElement('div');
+    containerTable.style.paddingLeft = '5%';
     
     const h2Tag = document.createElement('h2');
     h2Tag.innerHTML = 'Calcule su amortización con el método francés';
 
-    
-    let inputsList = [
-        {
-            labelFor:'monto',
-            labelInner:'Monto',
-            inputID: 'monto',
-            inputPH: 'Ingresar monto'
-        },
-        {
-            labelFor:'tiempo',
-            labelInner:'Meses',
-            inputID: 'tiempo',
-            inputPH: 'N° meses'
-        },
-        {
-            labelFor:'interes',
-            labelInner:'Monto',
-            inputID: 'monto',
-            inputPH: 'Tasa de interés mensual'
-        }
-    ];
+    containerForm.innerHTML=`
+                <h2>Calcular deuda 💰</h2>
+                <div class="form-group">
+                    <label for="monto">Monto</label>
+                    <input type="text" class="form-control" id="monto" placeholder="Ingresar monto">
+                </div>
+                <div class="form-group">
+                    <label for="tiempo">Meses</label>
+                    <input type="text" class="form-control" id="tiempo" placeholder="Ingresar cantidad de meses">
+                </div>
+                <div class="form-group">
+                    <label for="interes">Interés Mensual</label>
+                    <input type="text" class="form-control" id="interes" placeholder="Ingresar tasa de interés mensual">
+                </div>
+                <button type="submit" class="btn btn-primary" id="btnCalcular">Calcular</button>
+    `;
+    containerTable.innerHTML = `
+    <table id="lista-tabla" class="table">
+    <thead>
+        <tr>
+            <th>Fecha</th>
+            <th>Cuota</th>
+            <th>Capital</th>
+            <th>Interés</th>
+            <th>Saldo</th>
+        </tr>
+    </thead>
+    <tbody></tbody>
+    </table>
+    `;
 
-    let labelInner = ['Monto','Tiempo (en meses)', 'Interés mensual'];
-
-    let labelFor = ['monto','text','text'];
-    
     // appendChild
+    
     body.appendChild(container);
     container.appendChild(subContainer);
     subContainer.appendChild(containerForm);
-    containerForm.appendChild(h2Tag);
-    
-
-    inputsList.forEach(item =>{
-        const divForm = document.createElement('div');
-        divForm.classList.add=('form-group');    
-        const labelForm =document.createElement('label');
-        //labelForm.for
-        const inputForm = document.createElement('input');
-
-        //labelFor.=item;
-        
-        containerForm.appendChild(divForm)
-        divForm.appendChild(labelForm);
-        divForm.appendChild(inputForm);   
-    })
-
-
+    subContainer.appendChild(containerTable);
+   
 
     const buttonCalc = document.createElement('button');
     
     console.log('Funcó la vaina');
+  
 }
 
 
 document.addEventListener('DOMContentLoaded',loadScreen);
+
